6e69651b5e5ad101ba411f21e0220936ada90466
Sebasfb017 <sebastianf017@gmail.com>
2026-04-08 11:58:26 -0500

    Creacion autoguardado por si hay errores blindar la informacion

--- database_conn/connection.py
+++ database_conn/connection.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from contextlib import contextmanager
 
 # Definición de rutas base dinámicas (Sube un nivel para encontrar 'data')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,3 +14,20 @@ def db_conn():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.execute("PRAGMA journal_mode=WAL;")
     return conn
+
+@contextmanager
+def db_session():
+    """Context manager para la base de datos.
+    Asegura commit automático y cierra de forma segura la conexión."""
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR, exist_ok=True)
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn.execute("PRAGMA journal_mode=WAL;")
+    try:
+        yield conn
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        conn.close()
