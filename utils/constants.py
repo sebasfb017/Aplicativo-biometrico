4850d813c8cae67361a8c3ffb8fba9d6c8ce1776
Sebasfb017 <sebastianf017@gmail.com>
2026-05-29 08:38:29 -0500

    Se agregaron cargos dentro del area administrativa y dentro del area asistencial

--- utils/constants.py
+++ utils/constants.py
@@ -3,9 +3,10 @@
 ROLES = ("admin", "nomina")
 
 AREA_MAPPING = {
-    "Administrativo": ["Talento humano", "Calidad", "SST", "Dirección Administrativa", "Sistemas", "Servicios Generales", "Gerencia", "administrativa", "siau"],
+    "Administrativo": ["Talento humano", "Calidad", "SST", "Dirección Administrativa", "Sistemas", "Servicios Generales", "Gerencia", "administrativa", "siau", "Mercadeo y Publicidad", "Cirugía", "Jurídica", "Auditor Médico", "Orientador", "Seguridad"],
     "Financiera": ["Facturación", "Glosas", "Cartera", "Nomina", "Contabilidad"],
-    "Asistencial": ["Enfermería", "Farmacia"],
+    "Asistencial": ["Enfermería", "Farmacia", "Admisiones"],
     "Medico": ["Medico"],
     "Rayos X": ["Tecnólogo Rayos X"]
 }
+# Touch para recargar cache
