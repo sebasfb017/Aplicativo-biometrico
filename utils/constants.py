60c8016dac5a193f64a46a12768d8766596b9c8f
Sebasfb017 <sebastianf017@gmail.com>
2026-07-01 12:15:52 -0500

    Se realizo una correcion moviendo el cargo "Talento Humano" del area administrativa al area financiera

--- utils/constants.py
+++ utils/constants.py
@@ -3,8 +3,8 @@
 ROLES = ("admin", "nomina")
 
 AREA_MAPPING = {
-    "Administrativo": ["Talento humano", "Calidad", "SST", "Dirección Administrativa", "Sistemas", "Servicios Generales", "Gerencia", "administrativa", "siau", "Mercadeo y Publicidad", "Cirugía", "Jurídica", "Auditor Médico", "Orientador", "Seguridad", "Rehabilitación", "Archivo", "Control Interno"],
-    "Financiera": ["Facturación", "Glosas", "Cartera", "Nomina", "Contabilidad", "Dirección Financiera", "Compras"],
+    "Administrativo": ["Calidad", "SST", "Dirección Administrativa", "Sistemas", "Servicios Generales", "Gerencia", "administrativa", "siau", "Contratación, marketing y publicidad", "Marketing, publicidad y comunicaciones", "Ejecutivo comercial", "Cirugía", "Jurídica", "Auditor Médico", "Orientador", "Seguridad", "Rehabilitación", "Archivo", "Control Interno"],
+    "Financiera": ["Facturación", "Glosas", "Cartera", "Nomina", "Contabilidad", "Dirección Financiera", "Compras", "Talento humano"],
     "Asistencial": ["Enfermería", "Farmacia", "Admisiones"],
     "Medico": ["Medico"],
     "Rayos X": ["Tecnólogo Rayos X"]
